ce38ad84b91edc4cdc93e1de5c27b159c44f6177
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-08-27 18:10:29 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color,temperament,alive) VALUES
+(1, 'Mr. Chocolate', 12, 'F', 'Grey', 'Soft', 0),
+(2, 'Rowdy', 10, 'M', 'Black', 'Rough', 1),
+(3, 'Tabitha', 8, 'F', 'White', 'Friendly', 0),
+(4, 'Sergeant Brown', 14, 'M', 'Brown', 'Agressive', 1),
+(5, 'Melissa', 9, 'F', 'Pink', 'Calm', 1),
+(6, 'Grinch', 14, 'M', 'Indigo', 'Scary', 0),
+(7, 'Wendy', 10, 'F', 'Yellow', 'Chubby', 1),
+(8, NULL, 9, 'M', 'Grey', 'Agressive', 1);
